1b89577751aae07900b932c4fd0fcb8dea35cd6b
ArnaudXXIX <arnaudxxix@gmail.com>
2014-06-24 00:35:33 +0200

    Modify initialisation from parent

--- application/gui/mainwindow.py
+++ application/gui/mainwindow.py
@@ -10,6 +10,6 @@ class MainWindow(QtWidgets.QMainWindow):
     Main Window
     """
     def __init__(self):
-        # pylint: disable=non-parent-init-called,super-init-not-called
-        QtWidgets.QWidget.__init__(self, None)
+        # pylint: disable=non-parent-init-called
+        super().__init__()
 
