5a821ced642f46d6b1709c469cc2ade695a7f74c
Eijebong <eijebong@bananium.fr>
2014-08-01 23:56:54 +0200

    Update setup.py to add minors.

--- application/setup.py
+++ application/setup.py
@@ -11,6 +11,9 @@ FAK = faker.Faker()
 
 for i in range(100):
     id_ = api.notes.add(FAK.first_name(), FAK.first_name(), FAK.first_name(),
-                        FAK.email(), FAK.phone_number(), int(time.time()), "1A")
+                        FAK.email(), FAK.phone_number(),
+                        random.randint(int(time.time() - 36 * 365 * 3600 * 24),
+                                       int(time.time())),
+                        "1A")
     api.notes.transaction(id_, random.randint(-10, 10))
 
