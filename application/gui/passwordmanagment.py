841b89417241eb5d3006c03b6b6918b9fb118089
Eijebong <eijebong@bananium.fr>
2014-08-19 16:56:42 +0200

    Add forgotten files...

---
+++ application/gui/passwordmanagment.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2014 Bastien Orivel <b2orivel@enib.fr>
+# Copyright (C) 2014 Arnaud Levaufre <a2levauf@enib.fr>
+#
+# This file is part of Enibar.
+#
+# Enibar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Enibar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Enibar.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Password Managment Window
+=========================
+
+
+"""
+
+from PyQt5 import QtWidgets, uic
+import api.validator
+import gui.utils
+
+
+class PasswordManagment(QtWidgets.QDialog):
+    """ Password Managment main class
+    """
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui/passwordmanagment.ui', self)
+        self.pseudo_input.set_validator(api.validator.NAME)
+        self.old_password_input.set_validator(api.validator.NAME)
+        self.new_password_input.set_validator(api.validator.NAME)
+        self.on_change = api.validator.on_change(self)
+        self.show()
+
+    def accept(self):
+        """ Called when "changer" is clicked
+        """
+        if api.users.is_authorized(self.pseudo_input.text(),
+                                   self.old_password_input.text()):
+            api.users.change_password(self.pseudo_input.text(),
+                                      self.new_password_input.text())
+        else:
+            gui.utils.error("Erreur", "L'authentification a échouée.")
+        super().accept()
+
