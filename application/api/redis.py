3f930b463efec2b7a7d90d2ded5bee1e55607446
Eijebong <eijebong@bananium.fr>
2015-01-26 13:07:15 +0100

    Add settings for redis

--- application/api/redis.py
+++ application/api/redis.py
@@ -1,13 +1,14 @@
 import asyncio
 import asyncio_redis
 import json
+import settings
 
 connection = None
 
 
 def connect():
     global connection
-    connection = yield from asyncio_redis.Connection.create('localhost', 6379)
+    connection = yield from asyncio_redis.Connection.create(settings.HOST, 6379)
 
 
 def send_message(channel, message):
