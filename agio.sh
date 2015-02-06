62679389164f4211fead07c73cafe113b1597877
Eijebong <eijebong@bananium.fr>
2015-02-06 19:20:09 +0100

    Add python venv support to agio and auto mail

--- agio.sh
+++ agio.sh
@@ -17,5 +17,18 @@
 # along with Enibar.  If not, see <http://www.gnu.org/licenses/>.
 
 
-cd application
-python agio.py
+python3 -c 'import sys;(print("Python 3.4 or newer is required") and exit(1)) if sys.version_info < (3, 4) else exit(0)' || exit 1
+VENV="enibar-venv"
+DIR=$(dirname "$0")
+
+if [ -e "$DIR/$VENV" ]; then
+	PYTHON="../$VENV/bin/python3"
+else
+	echo ""
+	echo "WARNING: The venv does\'nt exist, you should probably"
+	echo "update the application by running ./update.sh"
+	echo ""
+	exit 1
+fi
+cd $DIR/application
+exec $PYTHON "-OO" "agio.py"
--- mail.sh
+++ mail.sh
@@ -16,5 +16,19 @@
 # You should have received a copy of the GNU General Public License
 # along with Enibar.  If not, see <http://www.gnu.org/licenses/>.
 
-cd application
-python mail.py
+
+python3 -c 'import sys;(print("Python 3.4 or newer is required") and exit(1)) if sys.version_info < (3, 4) else exit(0)' || exit 1
+VENV="enibar-venv"
+DIR=$(dirname "$0")
+
+if [ -e "$DIR/$VENV" ]; then
+	PYTHON="../$VENV/bin/python3"
+else
+	echo ""
+	echo "WARNING: The venv does\'nt exist, you should probably"
+	echo "update the application by running ./update.sh"
+	echo ""
+	exit 1
+fi
+cd $DIR/application
+exec $PYTHON "-OO" "mail.py"
