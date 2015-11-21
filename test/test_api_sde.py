6f89e7c4b37576b3da6d3000451e87f2bfae8d2f
Eijebong <eijebong@bananium.fr>
2015-11-21 17:57:30 +0100

    Add a test for _process_queue_item

--- test/test_api_sde.py
+++ test/test_api_sde.py
@@ -2,6 +2,32 @@ import basetest
 import api.redis
 import asyncio
 import json
+import settings
+
+
+settings.WEB_URL = 'http://127.0.0.1:52412/'
+
+
+class MockSdeServer(asyncio.Protocol):
+    received = None
+
+    def connection_made(self, transport):
+        self.transport = transport
+        self.to_send = b"HTTP/1.1 200 OK\r\n\r\n"
+
+    def data_received(self, data):
+        message = data.decode()
+        MockSdeServer.received = message
+
+        self.transport.write(self.to_send)
+
+        self.transport.close()
+
+
+class MockBadSdeServer(MockSdeServer):
+    def connection_made(self, transport):
+        super().connection_made(transport)
+        self.to_send = b"HTTP/1.1 500 OK\r\n\r\n"
 
 
 class ApiSdeTests(basetest.BaseTest):
@@ -27,6 +53,8 @@ class ApiSdeTests(basetest.BaseTest):
         self.loop.run_until_complete(api.redis.connect())
 
     def test_sde_add_note(self):
+        """ Testing adding a note to the queue
+        """
         async def func_test():
             async with api.redis.connection.get() as redis:
                 await redis.delete(api.sde.QUEUE_NAME)
@@ -43,6 +71,8 @@ class ApiSdeTests(basetest.BaseTest):
         self.loop.run_until_complete(task)
 
     def test_sde_delete_note(self):
+        """ Testing deleting a note from the queue
+        """
         async def func_test():
             async with api.redis.connection.get() as redis:
                 await redis.delete(api.sde.QUEUE_NAME)
@@ -59,6 +89,8 @@ class ApiSdeTests(basetest.BaseTest):
         self.loop.run_until_complete(task)
 
     def test_sde_add_history_lines(self):
+        """ Testing adding an history line to the queue
+        """
         async def func_test():
             async with api.redis.connection.get() as redis:
                 await redis.delete(api.sde.QUEUE_NAME)
@@ -77,6 +109,8 @@ class ApiSdeTests(basetest.BaseTest):
         self.loop.run_until_complete(task)
 
     def test_sde_delete_history_line(self):
+        """ Testing deleting an history line from the queue
+        """
         async def func_test():
             async with api.redis.connection.get() as redis:
                 await redis.delete(api.sde.QUEUE_NAME)
@@ -91,3 +125,48 @@ class ApiSdeTests(basetest.BaseTest):
 
         task = asyncio.ensure_future(func_test())
         self.loop.run_until_complete(task)
+
+    def test_sde_process_queue_item(self):
+        """ Testing processing one item of the queue
+        """
+        async def test_func():
+            await api.sde._process_queue_item(b'{"id": 1, "type": "history-delete"}')
+            msg = MockSdeServer.received.split('\r\n')
+            self.assertEqual(msg[0], 'DELETE /history HTTP/1.1')
+            self.assertEqual(json.loads(msg[-1]), {"id": 1})
+            await api.sde._process_queue_item(b'{"id": 3, "type": "note", "note": -2.0, "mail": "test2@pouette.com", "nickname": "test2"}')
+            msg = MockSdeServer.received.split('\r\n')
+            self.assertEqual(msg[0], 'PUT /note HTTP/1.1')
+            self.assertEqual(json.loads(msg[-1]), {"id": 3, "mail": "test2@pouette.com", "nickname": "test2", "note": -2.0})
+        coro = self.loop.create_server(MockSdeServer, '127.0.0.1', 52412)
+        server = self.loop.run_until_complete(coro)
+        task = asyncio.ensure_future(test_func())
+        self.loop.run_until_complete(task)
+        server.close()
+        self.loop.run_until_complete(server.wait_closed())
+
+    def test_sde_process_queue_item_without_server(self):
+        async def test_func():
+            with self.assertRaises(Exception):
+                await api.sde._process_queue_item(b'{"id": 1, "type": "history-delete"}')
+        task = asyncio.ensure_future(test_func())
+        self.loop.run_until_complete(task)
+
+    def test_sde_process_queue_item_with_bad_server(self):
+        async def test_func():
+            with self.assertRaises(api.sde.QueueProcessingException):
+                await api.sde._process_queue_item(b'{"id": 1, "type": "history-delete"}')
+            with self.assertRaises(api.sde.QueueProcessingException):
+                await api.sde._process_queue_item(b'{"id": 3, "type": "note", "note": -2.0, "mail": "test2@pouette.com", "nickname": "test2"}')
+        coro = self.loop.create_server(MockBadSdeServer, '127.0.0.1', 52412)
+        server = self.loop.run_until_complete(coro)
+        task = asyncio.ensure_future(test_func())
+        self.loop.run_until_complete(task)
+        server.close()
+        self.loop.run_until_complete(server.wait_closed())
+
+    def test_sde_process_queue(self):
+        """ Testing processing a full queue
+        """
+        pass
+
