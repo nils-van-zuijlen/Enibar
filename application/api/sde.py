93d90a7f617ba731aeb09ce6592eadb73676fd4b
Eijebong <eijebong@bananium.fr>
2015-11-24 18:16:30 +0100

    FREAKING PROXY !

--- application/api/sde.py
+++ application/api/sde.py
@@ -64,12 +64,14 @@ async def process_queue():
 async def _process_queue_item(item):
     parsed_item = json.loads(item.decode())
     type_ = parsed_item.pop('type')
+    if settings.USE_PROXY:
+        conn = aiohttp.ProxyConnector(proxy=settings.PROXY_AUTH)
     if type_.endswith('-delete'):
-        async with aiohttp.delete(settings.WEB_URL + type_[:-7], data=json.dumps(parsed_item)) as req:
+        async with aiohttp.delete(settings.WEB_URL + type_[:-7], connector=conn, data=json.dumps(parsed_item)) as req:
             if req.status != 200:
                 raise QueueProcessingException(req.status)
     else:
-        async with aiohttp.put(settings.WEB_URL + type_, data=json.dumps(parsed_item)) as req:
+        async with aiohttp.put(settings.WEB_URL + type_, connector=conn, data=json.dumps(parsed_item)) as req:
             if req.status != 200:
                 raise QueueProcessingException(req.status)
 
