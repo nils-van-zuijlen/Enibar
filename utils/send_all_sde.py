fe29ecda9547f104fc5661ed3938053652bf0bde
Eijebong <eijebong@bananium.fr>
2016-01-07 11:45:30 +0100

    Finally fix send_all_sde.py

--- utils/send_all_sde.py
+++ utils/send_all_sde.py
@@ -22,7 +22,7 @@ for trans in api.transactions.get():
         continue
 
     transaction = {'id': trans['id'],
-        'date': trans['date'],
+        'date': trans['date'].toPyDateTime().isoformat(),
         'category': trans['category'],
         'note': note[0]['nickname'],
         'product': trans['product'],
