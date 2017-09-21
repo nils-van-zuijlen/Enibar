ff6713a56c3c07ae59ee5f1599f8e8ad47ff42d3
Bastien Orivel <eijebong@bananium.fr>
2017-09-21 13:51:45 +0200

    Explicitely set the proxies URL in the photo synchronisation
    We were previously relying on the environnment variables to set the
    proxy URLs for us. Since we already have this URL in the settings, reuse
    it here too, this will avoid having multiple breakage points when the
    current used authentication will disappear.

--- application/photo_sync.py
+++ application/photo_sync.py
@@ -6,12 +6,20 @@ import os.path
 import api.redis
 import datetime
 
+if settings.USE_PROXY:
+    proxies = {
+        'http': settings.PROXY_AUTH,
+        'https': settings.PROXY_AUTH,
+    }
+else:
+    proxies = None
+
 last_updated = api.redis.get_key_blocking('photos_last_update')
 
 if last_updated != b'None':
-    photos = requests.get(settings.WEB_URL + "photos", params={'last_updated': last_updated}).json()
+    photos = requests.get(settings.WEB_URL + "photos", params={'last_updated': last_updated}, proxies=proxies).json()
 else:
-    photos = requests.get(settings.WEB_URL + "photos").json()
+    photos = requests.get(settings.WEB_URL + "photos", proxies=proxies).json()
 
 notes = api.notes.get()
 
@@ -20,7 +28,7 @@ for mail, photo in photos.items():
         if note['mail'] == mail:
             print(mail, photo)
             url = urljoin(settings.WEB_URL, "/static/medias/profile_pictures/" + photo)
-            data = requests.get(url).content
+            data = requests.get(url, proxies=proxies).content
             _, ext = os.path.splitext(photo)
             img_path = os.path.join(settings.IMG_BASE_DIR, note['mail'] + ext)
             with open(img_path, 'wb') as fd:
