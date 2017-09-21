b6db130f008b3356c2655a0ca6d6a99ac9eec23f
Bastien Orivel <eijebong@bananium.fr>
2017-09-21 13:58:25 +0200

    Split the users module in two
    The py.rs file will have the python interface functions and will
    disappear once the whole thing is rewritten. The mod.rs will contain the
    real Rust API.

--- application/rapi/src/users/mod.rs
+++ application/rapi/src/users/mod.rs
@@ -1,8 +1,9 @@
 pub mod models;
+mod py;
 
 use self::models::*;
-use bcrypt::{hash, verify, DEFAULT_COST};
-use cpython::{PyBool, PyModule, PyResult, Python};
+use bcrypt::{DEFAULT_COST, hash, verify};
+use cpython::{PyModule, Python};
 use diesel::prelude::*;
 use diesel;
 use errors::*;
@@ -10,7 +11,7 @@ use errors::ErrorKind::*;
 use schema::admins;
 use diesel::expression::AsExpression;
 use diesel::types::{BigInt, Bool};
-
+use self::py::*;
 
 impl User {
     /// Adds an user with default rights.
@@ -84,55 +85,6 @@ impl User {
 
         Ok(())
     }
-}
-
-pub fn py_add(py: Python, username: String, password: String) -> PyResult<PyBool> {
-    let conn = ::DB_POOL.get().unwrap();
-
-    match User::add(&*conn, &username, &password) {
-        Err(_) => Ok(PyBool::get(py, false)),
-        Ok(_) => Ok(PyBool::get(py, true)),
-    }
-}
-
-pub fn py_remove(py: Python, username: String) -> PyResult<PyBool> {
-    let conn = ::DB_POOL.get().unwrap();
-    let user = User::get(&*conn, &username);
-
-    if let Ok(user) = user {
-        match user.remove(&*conn) {
-            Err(_) => return Ok(PyBool::get(py, false)),
-            Ok(_) => return Ok(PyBool::get(py, true)),
-        }
-    }
-
-    Ok(PyBool::get(py, false))
-}
-
-pub fn py_is_authorized(py: Python, username: String, hash: String) -> PyResult<PyBool> {
-    let conn = ::DB_POOL.get().unwrap();
-    let user = User::get(&*conn, &username);
-
-    if let Ok(user) = user {
-        let is_authorized = user.is_authorized(&hash);
-        if let Ok(is_authorized) = is_authorized {
-            return Ok(PyBool::get(py, is_authorized));
-        }
-    }
-    return Ok(PyBool::get(py, false));
-}
-
-pub fn py_change_password(py: Python, username: String, new_password: String) -> PyResult<PyBool> {
-    let conn = ::DB_POOL.get().unwrap();
-    let user = User::get(&*conn, &username);
-
-    let is_ok = if let Ok(user) = user {
-        user.change_password(&*conn, &new_password).is_ok()
-    } else {
-        false
-    };
-
-    return Ok(PyBool::get(py, is_ok));
 }
 
 pub fn as_module(py: Python) -> PyModule {
