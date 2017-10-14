b352493037f768cabbc6a3c577368d6f4739db33
Bastien Orivel <eijebong@bananium.fr>
2017-10-14 12:11:10 +0200

    Rustfmt

--- application/rapi/rapi/src/validators.rs
+++ application/rapi/rapi/src/validators.rs
@@ -2,7 +2,7 @@ use validator::ValidationError;
 
 pub(crate) fn not_empty(data: &str) -> Result<(), ValidationError> {
     if data.trim() == "" {
-        return Err(ValidationError::new("This field cannot be empty".into()))
+        return Err(ValidationError::new("This field cannot be empty".into()));
     }
 
     Ok(())
