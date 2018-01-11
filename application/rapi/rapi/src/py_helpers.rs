7e236365af825e3fb0013f35f3622f5d89291b0f
Bastien Orivel <eijebong@bananium.fr>
2018-01-11 20:26:12 +0100

    Use lazy_static to import python modules in rapi::py_helpers
    This removes our direct usage of unsafe code and is way easier to grasp
    and copy/paste to add new imports.
    Instead of waiting for the git from the calls, we call
    Python::acquire_gil directly in the lazy_static initialization. I don't
    know why I didn't think about that earlier. This also remove the usage
    of the Box and the option but the benchmarks didn't show any improvement
    (nor regression) from that.

--- application/rapi/rapi/src/py_helpers.rs
+++ application/rapi/rapi/src/py_helpers.rs
@@ -4,31 +4,29 @@ use NaiveDate;
 use chrono::Datelike;
 use std::sync::{Once, ONCE_INIT};
 
-static DATE_ONCE: Once = ONCE_INIT;
-static mut DATE: Option<Box<PyObject>> = None;
-
-static DECIMAL_ONCE: Once = ONCE_INIT;
-static mut DECIMAL: Option<Box<PyObject>> = None;
+lazy_static! {
+    pub static ref DATE: PyObject = {
+        let gil = Python::acquire_gil();
+        let py = gil.python();
+        let datetime_module = py.import("datetime").expect("Can't import datetime");
+        let date = datetime_module.get(py, "date").unwrap();
+        date
+    };
+    pub static ref DECIMAL: PyObject = {
+        let gil = Python::acquire_gil();
+        let py = gil.python();
+        let decimal_module = py.import("decimal").expect("Can't import decimal");
+        let decimal = decimal_module.get(py, "Decimal").unwrap();
+        decimal
+    };
+}
 
 impl ToPyObject for BigDecimal {
     type ObjectType = PyObject;
 
     fn to_py_object(&self, py: Python) -> Self::ObjectType {
-        DECIMAL_ONCE.call_once(|| {
-            let decimal_module = py.import("decimal").expect("Can't import decimal");
-            let decimal = decimal_module.get(py, "Decimal").unwrap();
-            unsafe {
-                DECIMAL = Some(Box::new(decimal));
-            }
-        });
-
         let decimal = |object: &PyString| {
-            unsafe {
-                match DECIMAL {
-                    Some(ref decimal) => decimal.call(py, (object,), None).unwrap(),
-                    None => unreachable!(),
-                }
-            }
+            DECIMAL.call(py, (object,), None).unwrap()
         };
 
         return decimal(&format!("{}", self.0).into_py_object(py));
@@ -39,23 +37,9 @@ impl ToPyObject for NaiveDate {
     type ObjectType = PyObject;
 
     fn to_py_object(&self, py: Python) -> Self::ObjectType {
-        DATE_ONCE.call_once(|| {
-            let datetime_module = py.import("datetime").expect("Can't import datetime");
-            let date = datetime_module.get(py, "date").unwrap();
-            unsafe {
-                DATE = Some(Box::new(date));
-            }
-        });
-
         let date = |yyyy: i32, mm: u32, dd: u32| {
-            unsafe {
-                match DATE {
-                    Some(ref date) => date.call(py, (yyyy, mm, dd), None).unwrap(),
-                    None => unreachable!(),
-                }
-            }
+            DATE.call(py, (yyyy, mm, dd), None).unwrap()
         };
-
 
         date(self.year(), self.month(), self.day()).into_py_object(py)
     }
